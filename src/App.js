4dc10aed8ebb33bf128638ed5ff9345dfe877b4c
lee <wnsaud6016@naver.com>
2018-12-03 16:54:12 +0900

    styled-components add

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React, { Component, Fragment } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <Button danger="danger"/>
-        <Button />
-      </Fragment>
+      <Container>
+        <Button danger="danger" >danger</Button>
+        <Button >anything</Button>
+      </Container>
     );
   }
 }
-const Button =({danger})=>(
-  <button className={danger==="danger"?"button button--danger":"button button--success"} >hello</button>
-);
+
+const Container = styled.div`
+  height:100vh;
+  width:100%;
+  background-color:#efefef;
+`;
+
+
+const Button = styled.button`
+    border-radius: 50px;
+    padding: 5px;
+    min-width: 120px;
+    color: white;
+    font-weight: 600;
+    -webkit-appearance: none;
+    cursor: pointer;
+    &:active,
+    &:focus{
+      outline:none;
+    }
+    background-color:${props=>props.danger?"#e74c3c":"#2ecc71"}
+`;
 
 export default App;
